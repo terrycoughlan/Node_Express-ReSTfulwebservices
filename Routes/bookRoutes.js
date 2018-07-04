34cd287326bef07ebb593058ff5b66d6c922055d
Terry Coughlan <terence.coughlan@hpe.com>
2018-07-04 20:15:27 +0100

    Implemented verbs - PUT, PATCH and DELETE

--- Routes/bookRoutes.js
+++ Routes/bookRoutes.js
@@ -25,17 +25,63 @@ var routes = function (Book) {
                 }
             });
         });
-
+    bookRouter.use('/:bookId', function(req, res, next){ //middleware - do not repeat code more than twice if you do, then use middleware
+        Book.findById(req.params.bookId, function (err, book) { //callback
+            if(err) {
+                res.status(500).send(err)
+            }
+            else if(book) {
+                req.book = book;
+                next();
+            }
+            else {
+                res.status(404).send('No book found!!');
+            }
+        });
+    });
     bookRouter.route('/:bookId')
         .get(function (req, res) {
-
+            res.json(req.book);
+        })
+        .put(function (req, res){
             Book.findById(req.params.bookId, function (err, book) { //callback
+                req.book.title = req.body.title;
+                req.book.author = req.body.author;
+                req.book.genre = req.body.genre;
+                req.book.read = req.body.read;
+                req.book.save(function (err) {
+                    if(err) {
+                        res.status(500).send(err)
+                    }
+                    else{
+                        res.json(req.book);
+                    }
+                });
+            });
+        })
+        .patch(function (req, res){
+            if(req.body._id){ //because we don't want to update the book id
+                delete req.body._id;
+            }
+            for (var p in req.body){
+                req.book[p] = req.body[p];
+            }
+            req.book.save(function (err) {
                 if(err) {
-                    console.log(err);
                     res.status(500).send(err)
                 }
-                else {
-                    res.json(book);
+                else{
+                    res.json(req.book);
+                }
+            });
+        })
+        .delete(function (req, res){
+            req.book.remove(function (err) {
+                if(err) {
+                    res.status(500).send(err)
+                }
+                else{
+                    res.status(204).send('Removed');
                 }
             });
         });
